538ef022db647ec1e0203fe8cdb13fda3c2d22b0
nacho <56204692+nacho-1@users.noreply.github.com>
2022-05-21 13:58:05 -0300

    Agrego un comentario

--- src/block_terrain.h
+++ src/block_terrain.h
@@ -5,6 +5,15 @@
 #ifndef DUNE2000_BLOCKTERRAIN_H
 #define DUNE2000_BLOCKTERRAIN_H
 
+/*
+ * Siempre que se modifique este
+ * enum, verificar UnitMovility y
+ * todas sus clases hijas
+ *
+ * Tambien se podria impelementar
+ * con un patron visitor
+ * pero por ahora es mucho lio
+ */
 enum BlockTerrain {
 	sand,
 	dunes,
