62c426cf2c3718a71b1e653c9743da5dec2adaeb
nacho <56204692+nacho-1@users.noreply.github.com>
2022-05-31 16:57:29 -0300

    Modifico la firma de get_path para usar smart pointers en vez de punteros asi nomas

--- src/Model/terrain_map.h
+++ src/Model/terrain_map.h
@@ -12,6 +12,9 @@
 #include <list>
 #include <vector>
 #include <map>
+#include <memory>
+
+typedef std::shared_ptr<UnitMobility> UnitMobilityPtr;
 
 class TerrainMap {
 	BlockTerrain **map;
@@ -27,7 +30,7 @@ class TerrainMap {
 	 * Si la lista esta vacia es que no encontro ningun camino
 	 */
 	std::vector<BlockPosition>
-	get_path(BlockPosition origin, BlockPosition destination, const UnitMobility *mob) const;
+	get_path(BlockPosition origin, BlockPosition destination, const UnitMobilityPtr mob) const;
 
 	bool invalid_position(BlockPosition pos) const;
 
@@ -44,7 +47,7 @@ class TerrainMap {
 	bool already_visited(BlockPosition pos, const std::map<BlockPosition, PathNode> &visited) const;
 
 	std::list<BlockPosition>
-	get_neighbours(BlockPosition pos, const UnitMobility *mob) const;
+	get_neighbours(BlockPosition pos, const UnitMobilityPtr mob) const;
 
 	std::vector<BlockPosition>
 	build_path(const std::map<BlockPosition, PathNode> &visited, BlockPosition dst) const;
