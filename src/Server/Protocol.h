0f54b61d1a84980786c29fda67f6acbb8564e2e5
Agusgonza11 <aguselgonza@gmail.com>
2022-07-02 16:08:47 -0300

    protocol

--- src/Server/Protocol.h
+++ src/Server/Protocol.h
@@ -13,6 +13,20 @@ public:
     Protocol();
     int decodeLoginInstruction(Socket& socket, bool& was_closed);
     void sendMapList();
+
+    /*
+    void send_player_code(Socket &socket);
+
+    void send_terrain(Socket &socket);
+
+    void send_units(Socket &socket);
+
+    uint8_t receive_order(Socket &socket);
+
+    void receive_unit_move(Socket &socket);
+
+    void receive_unit_attack(Socket &socket);
+    */
 };
 
 
